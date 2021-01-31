cc440e3b233f39b382d73af6f08ac193f5be73f9
weishiba <185608130@qq.com>
2021-01-31 22:06:19 +0800

    添加消费者配置

--- dubbo_customer/src/main/java/com/wsc/service/impl/dohelloServiceImpl.java
+++ dubbo_customer/src/main/java/com/wsc/service/impl/dohelloServiceImpl.java
@@ -1,5 +1,7 @@
 package com.wsc.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import service.HelloService;
 import service.doHelloService;
 
@@ -7,9 +9,13 @@ import service.doHelloService;
  * 1，服务提供者注册到注册中心
  * 2.
  */
+@Service
 public class dohelloServiceImpl implements doHelloService {
+    @Autowired
     HelloService helloService;
+    @Override
     public String Hello(String name) {
+        System.out.println("姓名:"+name);
         return "服务消费者："+ helloService.Hello(name);
     }
 }
--- dubbo_provider/src/main/java/com/wsc/service/impl/helloServiceImpl.java
+++ dubbo_provider/src/main/java/com/wsc/service/impl/helloServiceImpl.java
@@ -3,6 +3,7 @@ package com.wsc.service.impl;
 import service.HelloService;
 
 public class helloServiceImpl implements HelloService {
+    @Override
     public String Hello(String name) {
         return name +":hello Dubbo!!!";
     }
